6d76cd41dc8ca0132dfa381c3586d225b40922f6
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-01-11 01:33:29 +0200

    edit: create movie class and tmdb service

---
+++ src/entities/Movie.ts
@@ -0,0 +1,36 @@
+import {v4 as uuid} from 'uuid';
+
+import {GenreJSON, MovieJSON} from '../services/tmdbService';
+
+export class Movie {
+  id: string = uuid();
+  movieID: number | null;
+  posterURL: string | null;
+  title: string | null;
+  releaseDate: Date | null;
+  genres: string[] | null;
+  voteAvg: number;
+  overview: string;
+
+  constructor(movieJSON: MovieJSON) {
+    this.movieID = movieJSON.id || null;
+    this.posterURL = movieJSON.backdrop_path || movieJSON.poster_path || null;
+    this.title = movieJSON.title || movieJSON.original_title || null;
+    this.releaseDate = this._initReleaseDate(movieJSON.release_date);
+    this.genres = this._initGenres(movieJSON.genres);
+    this.voteAvg = movieJSON.vote_average || 0;
+    this.overview = movieJSON.overview || '';
+  }
+
+  private _initReleaseDate(date: string | undefined) {
+    return date ? new Date(date) : null;
+  }
+
+  private _initGenres(genres: GenreJSON[] | undefined) {
+    return genres ? genres.map(genre => genre.name) : null;
+  }
+
+  getReleaseYear() {
+    return this.releaseDate?.getFullYear();
+  }
+}
---
+++ src/services/tmdbService.ts
@@ -0,0 +1,42 @@
+import {tmdbApiKey, tmdbClient, ContentCategory} from '../clients/tmdbClient';
+import {Movie} from '../entities/Movie';
+
+export interface GenreJSON {
+  id: number;
+  name: string;
+}
+
+export interface MovieJSON {
+  id?: number;
+  backdrop_path?: string;
+  poster_path?: string;
+  title?: string;
+  original_title?: string;
+  release_date?: string;
+  genres?: GenreJSON[];
+  vote_average?: number;
+  overview?: string;
+}
+
+export interface MoviesJSON {
+  results?: MovieJSON[];
+}
+
+const getMoviesByCategory = async (category: ContentCategory) => {
+  const {data} = await tmdbClient.get<MoviesJSON>(
+    `/movie/${category}?api_key=${tmdbApiKey}&language=en-US&page=1`,
+  );
+  return data.results?.map(movieJSON => new Movie(movieJSON));
+};
+
+const getMoviesByQuery = async (query: string) => {
+  const {data} = await tmdbClient.get<MoviesJSON>(
+    `/search/movie?api_key=${tmdbApiKey}&query=${query}&language=en-US&page=1`,
+  );
+  return data.results?.map(movieJSON => new Movie(movieJSON));
+};
+
+export default {
+  getMoviesByCategory,
+  getMoviesByQuery,
+};
