e6886999ec20b54dd2ade65e5dfde1a66641b038
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-01-11 23:28:48 +0200

    edit: handle no search results

--- src/components/MoviesSearchResultsList.tsx
+++ src/components/MoviesSearchResultsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList, Text} from 'react-native';
 
 import {Movie} from '../entities/Movie';
 import MoviesSearchResultsListItem from './MoviesSearchResultsListItem';
@@ -10,21 +10,38 @@ interface Props {
 
 const MoviesSearchResultsList = ({results}: Props) => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={results}
-        renderItem={({item}) => <MoviesSearchResultsListItem item={item} />}
-        keyExtractor={item => item.id}
-        showsVerticalScrollIndicator={false}
-        contentInsetAdjustmentBehavior="automatic"
-      />
-    </View>
+    <>
+      {/** empty results */}
+      {results.length === 0 && (
+        <View style={styles.emptyContainer}>
+          <Text>No Results!</Text>
+        </View>
+      )}
+
+      {results.length > 0 && (
+        <View style={styles.container}>
+          <FlatList
+            data={results}
+            renderItem={({item}) => <MoviesSearchResultsListItem item={item} />}
+            keyExtractor={item => item.id}
+            showsVerticalScrollIndicator={false}
+            contentInsetAdjustmentBehavior="automatic"
+          />
+        </View>
+      )}
+    </>
   );
 };
 
 export default MoviesSearchResultsList;
 
 const styles = StyleSheet.create({
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
   container: {
     flex: 1,
     paddingHorizontal: 5,
