645542be7b1d2b2ea569564316c0b161d4bfd0ee
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-03 01:55:41 +0300

    edit: user model

--- server/prisma/migrations/20230802225425_init/migration.sql
+++ server/prisma/migrations/20230802225425_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
