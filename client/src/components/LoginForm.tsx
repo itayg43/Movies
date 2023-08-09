18b258c283d491612206b55ad4df02ad28007ce4
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-10 01:29:20 +0300

    edit: create login form

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,85 @@
+import React, {useCallback, useState} from 'react';
+import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import {Button, TextInput} from 'react-native-paper';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import FormTextInput from '../components/FormTextInput';
+
+interface Props {
+  contentContainerStyle?: StyleProp<ViewStyle>;
+  onSubmit: (formData: LoginFormData) => void;
+}
+
+const loginFormSchema = z.object({
+  email: z
+    .string({required_error: 'Required'})
+    .email('Please enter a valid email address'),
+
+  password: z
+    .string({required_error: 'Required'})
+    .min(4, 'Password should be at least 4 characters long'),
+});
+
+export type LoginFormData = z.infer<typeof loginFormSchema>;
+
+const LoginForm = ({contentContainerStyle, onSubmit}: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: {isValid},
+  } = useForm<LoginFormData>({resolver: zodResolver(loginFormSchema)});
+
+  const [hidePassword, setHidePassword] = useState<boolean>(true);
+
+  const handleToggleHidePassword = useCallback(() => {
+    setHidePassword(currState => !currState);
+  }, [setHidePassword]);
+
+  return (
+    <View style={contentContainerStyle}>
+      {/** email */}
+      <FormTextInput
+        contentContainerStyle={styles.inputContainer}
+        style={styles.input}
+        control={control}
+        name="email"
+        label="Email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+
+      {/** password */}
+      <FormTextInput
+        contentContainerStyle={styles.inputContainer}
+        style={styles.input}
+        control={control}
+        name="password"
+        label="Password"
+        autoCapitalize="none"
+        secureTextEntry={hidePassword}
+        right={<TextInput.Icon icon="eye" onPress={handleToggleHidePassword} />}
+      />
+
+      {/** submit */}
+      <Button
+        mode="contained"
+        disabled={!isValid}
+        onPress={handleSubmit(onSubmit)}>
+        Submit
+      </Button>
+    </View>
+  );
+};
+
+export default LoginForm;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginBottom: 5,
+  },
+  input: {
+    marginBottom: 5,
+  },
+});
--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,40 +1,12 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet, Alert} from 'react-native';
-import {Button, TextInput} from 'react-native-paper';
-import {useForm} from 'react-hook-form';
-import {zodResolver} from '@hookform/resolvers/zod';
-import * as z from 'zod';
 
 import authService from '../services/authService';
 import errorHandlerUtil from '../utils/errorHandlerUtil';
 import SafeView from '../components/SafeView';
-import FormTextInput from '../components/FormTextInput';
-
-const loginFormSchema = z.object({
-  email: z
-    .string({required_error: 'Required'})
-    .email('Please enter a valid email address'),
-
-  password: z
-    .string({required_error: 'Required'})
-    .min(4, 'Password should be at least 4 characters long'),
-});
-
-type LoginFormData = z.infer<typeof loginFormSchema>;
+import LoginForm, {LoginFormData} from '../components/LoginForm';
 
 const LoginScreen = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: {isValid},
-  } = useForm<LoginFormData>({resolver: zodResolver(loginFormSchema)});
-
-  const [hidePassword, setHidePassword] = useState<boolean>(true);
-
-  const handleToggleHidePassword = useCallback(() => {
-    setHidePassword(currState => !currState);
-  }, [setHidePassword]);
-
   const handleLoginUser = useCallback(async (formData: LoginFormData) => {
     try {
       await authService.loginUser(formData.email, formData.password);
@@ -45,36 +17,7 @@ const LoginScreen = () => {
 
   return (
     <SafeView contentContainerStyle={styles.container}>
-      {/** email */}
-      <FormTextInput
-        contentContainerStyle={styles.inputContainer}
-        style={styles.input}
-        control={control}
-        name="email"
-        label="Email"
-        autoCapitalize="none"
-        keyboardType="email-address"
-      />
-
-      {/** password */}
-      <FormTextInput
-        contentContainerStyle={styles.inputContainer}
-        style={styles.input}
-        control={control}
-        name="password"
-        label="Password"
-        autoCapitalize="none"
-        secureTextEntry={hidePassword}
-        right={<TextInput.Icon icon="eye" onPress={handleToggleHidePassword} />}
-      />
-
-      {/** submit */}
-      <Button
-        mode="contained"
-        disabled={!isValid}
-        onPress={handleSubmit(handleLoginUser)}>
-        Submit
-      </Button>
+      <LoginForm onSubmit={handleLoginUser} />
     </SafeView>
   );
 };
@@ -84,12 +27,5 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-  },
-
-  inputContainer: {
-    marginBottom: 5,
-  },
-  input: {
-    marginBottom: 5,
   },
 });
