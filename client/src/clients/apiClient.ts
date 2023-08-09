88e00d02a83ca3d2990f6047a1fd82aac8ad4c4c
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-10 02:03:25 +0300

    edit: api client

--- client/src/clients/apiClient.ts
+++ client/src/clients/apiClient.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import {BACKEND_BASE_URL} from '@env';
+import tokenStorage from '../storage/tokenStorage';
 
 export enum ApiRoute {
   AUTH = '/auth',
@@ -9,5 +10,18 @@ export enum ApiRoute {
 const apiClient = axios.create({
   baseURL: `${BACKEND_BASE_URL}/api`,
 });
+
+apiClient.interceptors.request.use(
+  config => {
+    const token = tokenStorage.get('access');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
 
 export default apiClient;
