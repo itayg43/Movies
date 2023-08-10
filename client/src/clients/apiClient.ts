398e026b2727f209e9fd7dbd685823955669b0f2
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-10 03:22:57 +0300

    edit: api client

--- client/src/clients/apiClient.ts
+++ client/src/clients/apiClient.ts
@@ -14,13 +14,47 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
   async config => {
     const token = await tokenStorage.get('access');
+
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+
     return config;
   },
   error => {
     return Promise.reject(error);
+  },
+);
+
+apiClient.interceptors.response.use(
+  res => {
+    return res;
+  },
+  async err => {
+    const originalConfig = err.config;
+
+    if (originalConfig.url !== '/auth/login' && err.response) {
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+
+        try {
+          const {data} = await apiClient.post<{accessToken: string}>(
+            '/auth/reissue-access-token',
+            {
+              refreshToken: await tokenStorage.get('refresh'),
+            },
+          );
+
+          tokenStorage.set('access', data.accessToken);
+
+          return apiClient(originalConfig);
+        } catch (_error) {
+          return Promise.reject(_error);
+        }
+      }
+    }
+
+    return Promise.reject(err);
   },
 );
 
