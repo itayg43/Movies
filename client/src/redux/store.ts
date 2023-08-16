d0bcea1c82ace4538dabeb9b13b942b7e5a1c297
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-16 06:56:13 +0300

    edit: install redux flipper

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,10 +2,19 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import authReducer from './auth/authSlice';
 
+const additionalMiddlewares: any[] = [];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  additionalMiddlewares.push(createDebugger());
+}
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(additionalMiddlewares),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
