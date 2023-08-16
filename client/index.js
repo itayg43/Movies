3219e1c853e86dd769c9214249f1e9276dd38aad
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-16 03:58:53 +0300

    edit: install redux toolkit

--- client/index.js
+++ client/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {PaperProvider, MD3LightTheme as DefaultTheme} from 'react-native-paper';
+import {Provider as StoreProvider} from 'react-redux';
 import FlipperAsyncStorage from 'rn-flipper-async-storage-advanced';
 
 import App from './src/App';
 import {name as appName} from './app.json';
+import store from './src/redux/store';
 
 const theme = {
   ...DefaultTheme,
@@ -15,10 +17,12 @@ const theme = {
 const RootApp = () => {
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <FlipperAsyncStorage />
-        <App />
-      </NavigationContainer>
+      <StoreProvider store={store}>
+        <NavigationContainer>
+          <FlipperAsyncStorage />
+          <App />
+        </NavigationContainer>
+      </StoreProvider>
     </PaperProvider>
   );
 };
