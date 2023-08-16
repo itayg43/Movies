030620255d4f31ece43a4c289a9d1433f4e7cfc2
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-16 23:21:57 +0300

    edit: create request status

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,6 +2,8 @@ import * as z from 'zod';
 
 import {loginFormSchema, registerFormSchema} from '../schemas';
 
+export type RequestStatus = 'idle' | 'loading' | 'succeded' | 'failed';
+
 export type LoginFormData = z.infer<typeof loginFormSchema>;
 
 export type RegisterFormData = z.infer<typeof registerFormSchema>;
