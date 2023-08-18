fdf183b0642c84dfe42a8bee0ca09b8089d76772
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-19 02:41:36 +0300

    edit: delete auth dto

--- server/src/modules/auth/auth-controller.ts
+++ server/src/modules/auth/auth-controller.ts
@@ -7,24 +7,21 @@ import {
   LoginUserInput,
   ReissueUserAccessTokenInput,
 } from "./auth-schemas";
-import { UserDtoMapper } from "./auth-dto";
-
-const userDtoMapper = new UserDtoMapper();
 
 const registerUser = async (
   req: Request<{}, {}, RegisterUserInput>,
   res: Response
 ) => {
   const { name, email, password } = req.body;
-  const { user, tokens } = await authService.registerUser(
+  const { user, userTokens } = await authService.registerUser(
     name,
     email,
     password
   );
 
   res.status(statusCode.created).json({
-    user: userDtoMapper.map(user),
-    userTokens: tokens,
+    user,
+    userTokens,
   });
 };
 
@@ -33,11 +30,11 @@ const loginUser = async (
   res: Response
 ) => {
   const { email, password } = req.body;
-  const { user, tokens } = await authService.loginUser(email, password);
+  const { user, userTokens } = await authService.loginUser(email, password);
 
   res.status(statusCode.ok).json({
-    user: userDtoMapper.map(user),
-    userTokens: tokens,
+    user,
+    userTokens,
   });
 };
 
@@ -48,7 +45,9 @@ const reissueUserAccessToken = async (
   const { refreshToken } = req.body;
   const accessToken = authService.reissueUserAccessToken(refreshToken);
 
-  res.status(statusCode.ok).json({ accessToken });
+  res.status(statusCode.ok).json({
+    accessToken,
+  });
 };
 
 export default {
--- server/src/modules/auth/auth-dto.ts
+++
@@ -1,7 +0,0 @@
-import { DtoMapper } from "../../dtos";
-
-export class UserDtoMapper extends DtoMapper {
-  keys() {
-    return ["id", "name", "email", "createdAt", "updatedAt"];
-  }
-}
