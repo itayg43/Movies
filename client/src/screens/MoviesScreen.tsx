6c164640928b3764e7c653b76b16e72e6c835273
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-20 03:57:28 +0300

    edit: add temp logout btn

--- client/src/screens/MoviesScreen.tsx
+++ client/src/screens/MoviesScreen.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
 
+import {useAppDispatch} from '../hooks';
+import authActions from '../redux/auth/authActions';
 import SafeView from '../components/SafeView';
 
 const MoviesScreen = () => {
-  return <SafeView contentContainerStyle={styles.container}></SafeView>;
+  const dispatch = useAppDispatch();
+
+  const handleLogoutPress = useCallback(async () => {
+    dispatch(authActions.logoutUser());
+  }, [dispatch]);
+
+  return (
+    <SafeView contentContainerStyle={styles.container}>
+      <Button onPress={handleLogoutPress}>Logout</Button>
+    </SafeView>
+  );
 };
 
 export default MoviesScreen;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
