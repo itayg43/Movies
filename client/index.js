8bdf2a53cfd106e560f33e75f0d3c92fbc7e131b
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-20 04:57:19 +0300

    edit: create user storage

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import {AppRegistry} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {PaperProvider, MD3LightTheme as DefaultTheme} from 'react-native-paper';
 import {Provider as StoreProvider} from 'react-redux';
+import FlipperAsyncStorage from 'rn-flipper-async-storage-advanced';
 
 import App from './src/App';
 import {name as appName} from './app.json';
@@ -18,6 +19,7 @@ const RootApp = () => {
     <PaperProvider theme={theme}>
       <StoreProvider store={store}>
         <NavigationContainer>
+          <FlipperAsyncStorage />
           <App />
         </NavigationContainer>
       </StoreProvider>
