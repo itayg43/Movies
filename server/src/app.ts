cdba1b78e3e61c8c9a55de36f825f94d69a42786
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-23 03:15:15 +0300

    edit: install compression, set ttl to movies in redis

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import compression from "compression";
 import { createClient } from "redis";
 import { RedisClientType } from "@redis/client";
 
@@ -24,6 +25,7 @@ export let redisClient: RedisClientType;
 
 app.use(helmet());
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 
 app.use(requestLogger);
--- server/src/modules/movies/movies-cache-access.ts
+++ server/src/modules/movies/movies-cache-access.ts
@@ -2,16 +2,19 @@ import { redisClient } from "../../app";
 
 import { Movie } from "./tmdb/tmdb-service";
 
-const key = "movies";
+const HOUR_TTL = 60 * 60;
 
 const getMovies = async (): Promise<Movie[] | null> => {
-  const jsonValue = await redisClient.get(key);
+  const jsonValue = await redisClient.get("movies");
 
   return jsonValue ? JSON.parse(jsonValue) : null;
 };
 
 const setMovies = async (values: Movie[]) => {
-  await redisClient.set(key, JSON.stringify(values));
+  await redisClient.set("movies", JSON.stringify(values), {
+    EX: HOUR_TTL,
+    NX: true,
+  });
 };
 
 export default {
