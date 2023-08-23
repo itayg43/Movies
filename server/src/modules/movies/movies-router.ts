c455bdb709f8047eeebaa7b8bbbd6bc1636758f8
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-23 04:37:22 +0300

    edit: add validate access token middleware to get movies route

--- server/src/modules/movies/movies-router.ts
+++ server/src/modules/movies/movies-router.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 
+import validateAccessToken from "../../middlewares/validate-access-token";
 import moviesController from "./movies-controller";
 
 const mvoiesRouter = express.Router();
 
-mvoiesRouter.get("/", asyncHandler(moviesController.getMovies));
+mvoiesRouter.get(
+  "/",
+  [validateAccessToken],
+  asyncHandler(moviesController.getMovies)
+);
 
 export default mvoiesRouter;
