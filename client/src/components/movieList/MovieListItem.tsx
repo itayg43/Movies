e19e45d65a5e36cdd5843c5abb8594b1e31e2c07
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-26 00:25:37 +0300

    edit: image src, overview number of lines

--- client/src/components/movieList/MovieListItem.tsx
+++ client/src/components/movieList/MovieListItem.tsx
@@ -4,8 +4,6 @@ import {Card, Text} from 'react-native-paper';
 
 import {Movie} from '../../types';
 
-const THUMBNAIL_BASR_URL = 'https://image.tmdb.org/t/p/w500';
-
 type Props = {
   item: Movie;
   onPress: () => void;
@@ -14,12 +12,12 @@ type Props = {
 const MovieListItem = ({item, onPress}: Props) => {
   return (
     <Card mode="outlined" onPress={onPress}>
-      <Card.Cover source={{uri: `${THUMBNAIL_BASR_URL}${item.backdropPath}`}} />
+      <Card.Cover source={{uri: item.backdropUrl}} />
 
       <Card.Title title={item.title} titleStyle={styles.title} />
 
       <Card.Content>
-        <Text variant="bodyMedium" numberOfLines={3}>
+        <Text variant="bodyMedium" numberOfLines={2}>
           {item.overview}
         </Text>
       </Card.Content>
