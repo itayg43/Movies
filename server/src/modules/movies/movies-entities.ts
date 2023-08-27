dd3d257c12439fe6e1b2cc539b1e68c85a340a30
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-27 06:28:38 +0300

    edit: stream line movie, and movie details

--- server/src/modules/movies/movies-entities.ts
+++ server/src/modules/movies/movies-entities.ts
@@ -20,45 +20,28 @@ export class Movie {
   constructor(data: MovieResponseData) {
     this.id = data.id;
     this.title = data.title;
-    this.overview = data.overview;
 
     this.backdropUrl = data.backdrop_path
       ? `${TMDB_BACKDROP_BASE_URL}${data.backdrop_path}`
       : null;
 
     this.posterUrl = `${TMDB_POSTER_BASE_URL}${data.poster_path}`;
+    this.overview = data.overview;
     this.releaseDate = data.release_date;
     this.voteAverage = data.vote_average;
   }
 }
 
-export class MovieDetails {
-  id: number;
-  title: string;
-  backdropUrl: string | null;
-  posterUrl: string;
-  overview: string;
+export class MovieDetails extends Movie {
   genres: string[];
-  releaseDate: string;
-  voteAverage: number;
   youTubeTrailerUrl: string | null;
   recommendations: Movie[];
 
   constructor(data: MovieDetailsResponseData) {
-    this.id = data.id;
-    this.title = data.title;
-    this.overview = data.overview;
+    super(data);
 
-    this.backdropUrl = data.backdrop_path
-      ? `${TMDB_BACKDROP_BASE_URL}${data.backdrop_path}`
-      : null;
-
-    this.posterUrl = `${TMDB_POSTER_BASE_URL}${data.poster_path}`;
     this.genres = data.genres.map((g) => g.name);
-    this.releaseDate = data.release_date;
-    this.voteAverage = data.vote_average;
     this.youTubeTrailerUrl = this._initYouTubeTrailerUrl(data.videos.results);
-
     this.recommendations = data.recommendations.results.map(
       (r) => new Movie(r)
     );
--- server/src/modules/movies/tmdb/tmdb-service.ts
+++ server/src/modules/movies/tmdb/tmdb-service.ts
@@ -37,78 +37,26 @@ export default {
 };
 
 export type GetMoviesResponseData = {
-  page: number;
   results: MovieResponseData[];
-  total_pages: number;
-  total_results: number;
 };
 
 export type MovieResponseData = {
-  adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
   id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
   title: string;
-  video: boolean;
+  backdrop_path: string | null;
+  poster_path: string;
+  overview: string;
+  release_date: string;
   vote_average: number;
-  vote_count: number;
 };
 
-export type MovieDetailsResponseData = {
-  adult: boolean;
-  backdrop_path: string | null;
-  belongs_to_collection: null;
-  budget: number;
+export type MovieDetailsResponseData = MovieResponseData & {
   genres: [
     {
       id: number;
       name: string;
     }
   ];
-  homepage: string;
-  id: number;
-  imdb_id: string;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  production_companies: [
-    {
-      id: number;
-      logo_path: string;
-      name: string;
-      origin_country: string;
-    }
-  ];
-  production_countries: [
-    {
-      iso_3166_1: string;
-      name: string;
-    }
-  ];
-  release_date: string;
-  revenue: number;
-  runtime: number;
-  spoken_languages: [
-    {
-      english_name: string;
-      iso_639_1: string;
-      name: string;
-    }
-  ];
-  status: string;
-  tagline: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
   videos: {
     results: VideoResponseData[];
   };
@@ -116,14 +64,8 @@ export type MovieDetailsResponseData = {
 };
 
 export type VideoResponseData = {
-  iso_639_1: string;
-  iso_3166_1: string;
-  name: string;
   key: string;
   site: "YouTube";
-  size: number;
   type: "Trailer";
   official: boolean;
-  published_at: string;
-  id: string;
 };
