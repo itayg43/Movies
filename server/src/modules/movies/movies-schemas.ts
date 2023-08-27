a2b6d4faae2d34f05508fdc19f44d3d3e2a20af2
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-27 07:19:08 +0300

    edit: allow string with numbers only

--- server/src/modules/movies/movies-schemas.ts
+++ server/src/modules/movies/movies-schemas.ts
@@ -4,7 +4,7 @@ export const getMovieDetailsSchema = object({
   params: object({
     id: string({
       required_error: "Movie id is required",
-    }),
+    }).regex(/^\d+$/),
   }),
 });
 export type GetMovieDetailsInput = TypeOf<
