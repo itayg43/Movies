8cb593e36bce652248fc0078fb6ac43f4deede57
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-08-29 04:06:01 +0300

    edit: id param should include numbers only

--- server/src/modules/movies/movies-schemas.ts
+++ server/src/modules/movies/movies-schemas.ts
@@ -4,7 +4,7 @@ export const getMovieDetailsSchema = object({
   params: object({
     id: string({
       required_error: "Movie id is required",
-    }),
+    }).regex(/^\d+$/, "Movie id should include number only"),
   }),
 });
 export type GetMovieDetailsInput = TypeOf<
