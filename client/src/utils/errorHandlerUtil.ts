aa5236f63891830d07611e77a5be80ae2034a30e
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-09-01 06:06:53 +0300

    edit: remove console log

--- client/src/utils/errorHandlerUtil.ts
+++ client/src/utils/errorHandlerUtil.ts
@@ -4,10 +4,6 @@ const DEFAULT_MESSAGE =
   'Something went wrong, please try again in a few minutes or contact support';
 
 const extractMessage = (error: any): string => {
-  if (__DEV__) {
-    console.error(error);
-  }
-
   const message =
     error instanceof AxiosError ? error.response?.data.message : error.message;
   return message ?? DEFAULT_MESSAGE;
