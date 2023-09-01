13505a4e395b32f06ef0bc0b32f67beca0a77632
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-09-01 04:40:14 +0300

    edit: remove status

--- client/src/redux/auth/authSelectors.ts
+++ client/src/redux/auth/authSelectors.ts
@@ -1,7 +1,5 @@
 import {RootState} from '../store';
 
-export const selectAuthStatus = (state: RootState) => state.auth.status;
+export const selectAuthUser = (state: RootState) => state.auth.user;
 
 export const selectAuthMessage = (state: RootState) => state.auth.message;
-
-export const selectAuthUser = (state: RootState) => state.auth.user;
--- client/src/redux/auth/authSlice.ts
+++ client/src/redux/auth/authSlice.ts
@@ -1,81 +1,54 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-import {RequestStatus, User} from '../../types';
+import {User} from '../../types';
 import authAsyncActions from './authAsyncActions';
 
 type AuthState = {
-  status: RequestStatus;
-  message: string;
   user: User | null;
+  message: string;
 };
 
 const initialState: AuthState = {
   user: null,
-  status: 'idle',
   message: '',
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    resetAuthStatusAndMessage: state => {
-      state.status = 'idle';
-      state.message = '';
-    },
-  },
+  reducers: {},
   extraReducers: builder => {
     builder
       // login
-      .addCase(authAsyncActions.loginUser.pending, state => {
-        state.status = 'loading';
-        state.message = '';
-      })
       .addCase(authAsyncActions.loginUser.fulfilled, (state, {payload}) => {
-        state.status = 'succeded';
         state.user = payload;
       })
       .addCase(authAsyncActions.loginUser.rejected, (state, {payload}) => {
-        state.status = 'failed';
         if (payload) {
           state.message = payload;
         }
       })
 
       // register
-      .addCase(authAsyncActions.registerUser.pending, state => {
-        state.status = 'loading';
-        state.message = '';
-      })
       .addCase(authAsyncActions.registerUser.fulfilled, (state, {payload}) => {
-        state.status = 'succeded';
         state.user = payload;
       })
       .addCase(authAsyncActions.registerUser.rejected, (state, {payload}) => {
-        state.status = 'failed';
         if (payload) {
           state.message = payload;
         }
       })
 
       // logout
-      .addCase(authAsyncActions.logoutUser.pending, state => {
-        state.status = 'loading';
-        state.message = '';
-      })
       .addCase(authAsyncActions.logoutUser.fulfilled, state => {
-        state.status = 'succeded';
         state.user = null;
       })
       .addCase(authAsyncActions.logoutUser.rejected, (state, {payload}) => {
-        state.status = 'failed';
         if (payload) {
           state.message = payload;
         }
       });
   },
 });
-
-export const {resetAuthStatusAndMessage} = authSlice.actions;
 
 export default authSlice.reducer;
